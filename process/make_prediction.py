e2c1c8ab446f6f24d35357ec3300fb9f9baa83e1
halalc <31180187+halalc@users.noreply.github.com>
2018-10-31 09:01:01 +0800

    Update make_prediction.py

--- process/make_prediction.py
+++ process/make_prediction.py
@@ -1,28 +1,30 @@
 import utils
 from collections import defaultdict
 import numpy as np
+import pandas as pd
+import sys
+import json
+import argparse
+import configparser
 from keras.utils import np_utils
 from keras.preprocessing import sequence
-import pandas as pd
-import os
-import sys
-import argparse
 
-
-#get the course name from command line
+#get the course name and start date of the course from command line
 parser = argparse.ArgumentParser()
-parser.add_argument("course_name", help="input the course you want to make prediction",)
+parser.add_argument("course_name", help="input the course you want to make prediction")
+parser.add_argument("start_date", help="input the start date of the course in this format: YYYY-MM-DD")
+parser.add_argument("end_date", help="input the end date of the course in this format: YYYY-MM-DD")
 args = parser.parse_args()
 course=arges.course_name
+start_date=datetime.datetime.strptime(arges.start_date, '%Y-%m-%d')
+start_date=datetime.datetime.strptime(arges.start_date, '%Y-%m-%d')
 
 # get the data directory
 home_dir=path.dirname(os.getcwd())
-model_dir=home_dir+'/model'
 data_dir=home_dir+'/data'
-event_list_file=data_dir+'/RNN_event_list.csv'
 log_file=data_dir+ '/'+course+'-event.log'
 predictions_file=data_dir+'/predictions.csv'
-time_interval_file=data_dir+ 'time_intervals.json'
+cut_off_file=data_dir+ 'cut_off.json'
 config_file=home_dir+'/config.ini'
 
 config = ConfigParser.ConfigParser()
@@ -30,32 +32,23 @@ config.read(config_file)
 
 # Generate dictionary of student to list of his/her actions sorted chronologically
 student_sorted=utils.generate_ordered_event_copy(log_file)
-
 # Generate dictionary of course event to integer encoding
-ce_types = utils.get_ce_types(event_list_file)
-
+ce_types = utils.get_ce_types()
 # get the pre_action list for events
 pre_action = config.get('Options', 'preaction')
 
-
 print('data processing')
 
-#get the date
-date=utils.get_date(course,data_dir)
-start_time=date[0]
-end_time=date[1]
 #make sure the log is processing the "real time" log
 now=datetime.datetime.now()
 if now<end_time:
     end_time=now
-
 #get the cutoffs
-cutoff=utils.get_cutoff(time_interval_file)
-
-#initiate the user user_sequence list
+with open(cut_off_file, 'r') as f:
+    time_interval=json.load(f)  
+#initiate the user and user_sequence list
 users_ID=[]
 users_seq=[]
-
 #get the sequence
 for user,actions in student_sorted.items():
     u_seq=[]
@@ -70,7 +63,6 @@ for user,actions in student_sorted.items():
             continue
         if parsed_event in ce_types and time_element<end_time:
             temp.append((ce_types[parsed_event]+2, time_element))
-
     #change the category by time intervals    
     if len(temp)==1:
         u_seq=temp
@@ -100,8 +92,7 @@ for user,actions in student_sorted.items():
                 u_seq.append((event+2*77,time))
             else:
                 u_seq.append((event+3*77,time))
-                
-
+    #add the time markers
     weekend=start_time
     # add weekends
     while(1):
@@ -109,7 +100,6 @@ for user,actions in student_sorted.items():
             break
         u_seq.append((0, weekend))
         weekend = weekend+ datetime.timedelta(days=7)
-
     # add days
     dayend=start_time
     while(1):
@@ -117,15 +107,12 @@ for user,actions in student_sorted.items():
         if dayend>end_time:
             break
         u_seq.append((1, dayend))
-
     # sort again
     s = sorted(u_seq, key=lambda p: p[1])
     seq= [item[0] for item in s]
-    
     #generate the list
     users_seq.append(seq)
     users_ID.append(user)
-
 event_df = pd.DataFrame({'userID': users_ID, 'seq':users_seq})
 
 print('making predictions')
@@ -134,18 +121,14 @@ print('making predictions')
 max_seq_len = int(config.get('Options', 'max_seq_len'))
 max_input_dim = int(config.get('Options', 'max_input_dim'))
 batch_size = int(config.get('Options', 'batch_size'))
-
 event_df.reset_index(drop=True, inplace=True)
 event_df.reindex(np.random.permutation(event_df.index))
 event_list = event_df['seq'].values
-
 event_list_binary = [np_utils.to_categorical(x, max_input_dim) for x in event_list]
 x_train = sequence.pad_sequences(event_list_binary, maxlen=max_seq_len, dtype='bool',
                                      padding='post', truncating='post')
-
-
 # Load model weights and get predictions
-model = utils.load_keras_weights_from_disk(model_dir)
+model = utils.load_keras_weights_from_disk()
 y_pred = model.predict(x_test, batch_size=batch_size, verbose=1)
 y_pred = y_pred[:, -1, :]
 y_pred_pass = y_pred[event_df, 0]
@@ -153,9 +136,8 @@ y_pred_comp = y_pred[event_df, 1]
 y_pred_stop = y_pred[event_df, 2]
 result_df = pd.DataFrame({'userID': users_ID, 'y_pass':y_pred_pass,'y_comp':y_pred_comp,'y_stop':y_pred_stop})
 
+print("saving the prediction in data directory")
 
 #generate the prediciton file
 header = ["User_id", "pass_prediction", "completion_prediction", "stopout_prediction"]
 result.to_csv(predictions_file, index=False, columns = header)
-print("saving the prediction in data directory")
-
